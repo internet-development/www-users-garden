6b1b214fd89b17a560fc4f55395cb46ffd9cda75
jimmylee <send@jimmy.email>
2024-08-27 03:25:13 -0700

    updates SEO image

--- components/Page.tsx
+++ components/Page.tsx
@@ -18,13 +18,13 @@ export default function Page(props) {
         <meta property="og:url" content={props.url} />
         <meta property="og:title" content={props.title} />
         <meta property="og:description" content={props.description} />
-        <meta property="og:image" content="https://intdev-global.s3.us-west-2.amazonaws.com/public/internet-dev/ccb2bcf8-9ba4-4976-9403-f3dac8ce0047.png" />
+        <meta property="og:image" content="https://intdev-global.s3.us-west-2.amazonaws.com/public/internet-dev/7ac4ff7d-aa70-4da5-99b2-5d8f7f34ce04.png" />
 
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content={props.url} />
         <meta property="twitter:title" content={props.title} />
         <meta property="twitter:description" content={props.description} />
-        <meta property="twitter:image" content="https://intdev-global.s3.us-west-2.amazonaws.com/public/internet-dev/ccb2bcf8-9ba4-4976-9403-f3dac8ce0047.png" />
+        <meta property="twitter:image" content="https://intdev-global.s3.us-west-2.amazonaws.com/public/internet-dev/7ac4ff7d-aa70-4da5-99b2-5d8f7f34ce04.png" />
 
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
